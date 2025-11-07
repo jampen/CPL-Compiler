8d4da8ee031c36d143ab228772d3727bcb11e0a2
Jamie Pendergast <jamiependergast@gmail.com>
2025-11-07 15:08:57 +0000

    Fixed git automatically ignoring the x64 folder by renaming it

---
+++ BE/x64/X64Allocator.cs
@@ -0,0 +1,51 @@
+﻿namespace CPL.BE;
+
+internal class X64Allocator : IAllocator
+{
+    public MemoryLocation Allocate(IR.Variable variable)
+    {
+        /// TODO:
+        return new StackLocation(0);
+    }
+
+    public int SizeOfType(IR.Type type)
+    {
+        if (type is IR.IntType intType)
+        {
+            return intType.BitWidth / 8;
+        }
+
+        if (type is IR.PointerType)
+        {
+            // On x64 pointers are a QWORD
+            return 8;
+        }
+
+        if (type is IR.ArrayType arrayType)
+        {
+            return SizeOfType(arrayType.ElementType) * arrayType.Size;
+        }
+
+        if (type is IR.VoidType)
+        {
+            return 0;
+        }
+
+        throw new NotImplementedException(nameof(type));
+    }
+
+    public string TranslateMemoryLocation(MemoryLocation location)
+    {
+        if (location is StackLocation stackLocation)
+        {
+            return $"[rbp-{stackLocation.StackOffset}]";
+        }
+        else if (location is RegisterLocation reg)
+        {
+            return reg.Name;
+        }
+
+        throw new NotImplementedException(nameof(location));
+    }
+
+}
---
+++ BE/x64/X64Backend.cs
@@ -0,0 +1,76 @@
+﻿namespace CPL.BE;
+
+internal sealed class X64Backend() : Backend(new X64Allocator())
+{
+    public override void Generate(IR.Context context)
+    {
+        WriteLine("section .text");
+        foreach (var functionDefinition in context.Functions)
+        {
+            IR.Function function = functionDefinition.Value;
+            WriteLine($"global {function.Name}");
+            WriteLine($"{function.Name}:");
+            // Prologue
+            WriteLine("\tpush rbp");
+            WriteLine("\tmov rbp, rsp");
+            WriteLine($"\tsub rsp, {Align16(CalculateStackSize(function.Block))}");
+            EmitInstruction(function.Block);
+            // Epilogue
+            WriteLine("\tmov rsp, rbp");
+            WriteLine("\tpop rbp");
+            WriteLine("\tret");
+        }
+    }
+
+
+    private void WriteLine(string s)
+    {
+        Console.WriteLine(s);
+    }
+
+    private void Load(IR.Load load)
+    {
+        var destinationLocation = MemoryLocations[load.Destination];
+        var sourceLocation = MemoryLocations[load.Source];
+
+        var translatedDestination = Allocator.TranslateMemoryLocation(destinationLocation);
+        var translatedSource = Allocator.TranslateMemoryLocation(sourceLocation);
+
+        WriteLine($"\tmov {translatedDestination}, {translatedSource}");
+        MemoryLocations[load.Destination] = destinationLocation;
+    }
+
+    private void Store(IR.Store store)
+    {
+        var destinationLocation = MemoryLocations[store.Destination];
+
+        if (store.Source is IR.Constant constant)
+        {
+            string type = "unset";
+
+            if (constant.Type is IR.IntType)
+            {
+                type = "dword";
+            }
+
+            WriteLine($"\tmov {type} {Allocator.TranslateMemoryLocation(destinationLocation)}, {constant.Value}");
+        }
+    }
+
+    private void EmitInstruction(IR.Instruction ins)
+    {
+        switch (ins)
+        {
+            case IR.Block block:
+                block.Instructions.ForEach(EmitInstruction);
+                break;
+
+            case IR.DeclareVariable declareVariable:
+                MemoryLocations[declareVariable.Variable] = Allocator.Allocate(declareVariable.Variable);
+                break;
+
+            case IR.Store store: Store(store); break;
+            case IR.Load load: Load(load); break;
+        }
+    }
+}
